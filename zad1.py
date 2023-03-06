9f6b7095ce42ceacdddb31ba4558462b3f4dd59d
KarinaYarmosh <91902398+KarinaYarmosh@users.noreply.github.com>
2023-03-06 13:10:55 +0100

    Edit, add image

--- zad1.py
+++ zad1.py
@@ -1,4 +1,4 @@
-#przez okno po nacisnieciu przycisku "Start" zaladowac plik z komputera, zparsowac go i wyswietlic w konsole
+#przez okno po nacisnieciu przycisku "Start" zaladowac plik z komputera, zparsowac go i wyswietlic w oknie, zmieniajac liczby na pikseli
 from tkinter import *
 from tkinter import filedialog
 
@@ -9,7 +9,6 @@ class Window(Frame):
         self.format = None
         self.szerokosc = None
         self.wysokosc = None
-        self.sprawdzenie = 0
         self.master = master
         self.init_window()
 
@@ -21,6 +20,31 @@ class Window(Frame):
         #przycisk start, po nacisnieciu otwiera okno z wyborem pliku
         startButton = Button(self, text="Start", command=self.client_start)
         startButton.place(x=0, y=30)
+        # Desaturacja obrazu
+        # Negatyw(kolor dopełniający)
+        # Kontrast - liniowy, logarytmiczny, potęgowy
+        # Jasność(z zachowaniem koloru)
+        # Nasycenie
+        # Suma, różnica i iloczyn obrazów
+        # Ogólne przekształcenie na kanale monochromatycznym
+        desaturacjaButton = Button(self, text="Desaturacja", command=self.desaturacja)
+        desaturacjaButton.place(x=0, y=60)
+        negatywButton = Button(self, text="Negatyw", command=self.negatyw)
+        negatywButton.place(x=0, y=90)
+        kontrastButton = Button(self, text="Kontrast")
+        kontrastButton.place(x=0, y=120)
+        jasnoscButton = Button(self, text="Jasność")
+        jasnoscButton.place(x=0, y=150)
+        nasycenieButton = Button(self, text="Nasycenie")
+        nasycenieButton.place(x=0, y=180)
+        sumaButton = Button(self, text="Suma")
+        sumaButton.place(x=0, y=210)
+        roznicaButton = Button(self, text="Różnica")
+        roznicaButton.place(x=0, y=240)
+        iloczynButton = Button(self, text="Iloczyn")
+        iloczynButton.place(x=0, y=270)
+        ogolneButton = Button(self, text="Ogólne")
+        ogolneButton.place(x=0, y=300)
 
     def client_start(self):
         #otwiera okno z wyborem pliku
@@ -53,11 +77,45 @@ class Window(Frame):
                 print("Wysokosc: " + self.wysokosc)
             else:
                 print(self.line)
+        #wyswietlic obrazki w oknie, zmieniajac liczby na pikseli w formatach PBM, PGM, PPM
+        #wyswietlenie obrazka w oknie
+        #dziala tylko dla ppm
+        self.canvas = Canvas(self, width=self.szerokosc, height=self.wysokosc)
+        self.canvas.pack()
+        self.img = PhotoImage(file=self.filename)
+        self.canvas.create_image(20, 20, anchor=NW, image=self.img)
 
+    def desaturacja(self):
+        print("Desaturacja")
+
+    def negatyw(self):
+        print("Negatyw")
+
+    def kontrast(self):
+        print("Kontrast")
+        
+    def jasnosc(self):
+        print("Jasność")
+        
+    def nasycenie(self):
+        print("Nasycenie")
+        
+    def suma(self):
+        print("Suma")
+        
+    def roznica(self):
+        print("Różnica")
+        
+    def iloczyn(self):
+        print("Iloczyn")
+        
+    def ogolne(self):
+        print("Ogólne")
+        
     def client_exit(self):
         exit()
 
 root = Tk()
-root.geometry("400x300")
+root.geometry("500x500")
 app = Window(root)
 root.mainloop()
